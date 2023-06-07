d40e039b6fb1cf4b547913b4aca8bf5988f71f32
Ali Nikaein <108432369+Ali-Nikaein@users.noreply.github.com>
2023-06-07 16:56:24 +0330

    Create UnitTest_alipyTest2.py

---
+++ UnitTest_alipyTest2.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+def add_numbers(a, b):
+    return a + b
+
+  
+class TestAddNumbers(unittest.TestCase):
+  
+    def test_addition(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+
+        
+    def test_negative_numbers(self):
+        self.assertEqual(add_numbers(-5, 10), 5)
+
+        
+    def test_zero(self):
+        self.assertEqual(add_numbers(0, 0), 0)
+
+        
+if __name__ == '__main__':
+    unittest.main()
