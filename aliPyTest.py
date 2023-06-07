590794b7f6da4bac02efddc6c6bcf42d6b823094
Ali Nikaein <108432369+Ali-Nikaein@users.noreply.github.com>
2023-06-07 13:16:32 +0330

    Create aliPyTest.py

---
+++ aliPyTest.py
@@ -0,0 +1,7 @@
+print("Hello guys!")
+a = int(input("Please enter the first number: "))
+b = int(input("Please enter the second number: "))
+c = a + b
+print("The first number was:", a)
+print("The second number was:", b)
+print("The sum of them is:", c)
