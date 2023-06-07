1104ff6afa2a768a6f753dd5c0d44e85ffd712e3
Ali Nikaein <108432369+Ali-Nikaein@users.noreply.github.com>
2023-06-07 12:46:01 +0330

    Create cppTest.cpp
    this program get 2 numbers from input and add them together then print it

---
+++ CppActionTest/cppTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int args, char* argv[])
+{
+    cout<<"Hello guys !"<<endl;
+    int a , b , c;
+    cout<<"please enter two numbers :"<<endl;
+    cin>>a>>b;
+    c = a + b;
+    cout<<"firts number was : "<< a <<"\nsecond number was : "<< b <<"\nand sum of them is : "<< c <<endl;
+    return 0;
+}
